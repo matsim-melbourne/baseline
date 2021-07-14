0ed827e62ee396ebe27a0a69fd4470debaab6a11
jafshin <jxafshin@gmail.com>
2021-07-14 23:35:05 +1000

    using sub-population and moving bicycle to config.xml

--- src/main/java/io/github/matsimmelbourne/baseline/run/RunBaselineScenario.java
+++ src/main/java/io/github/matsimmelbourne/baseline/run/RunBaselineScenario.java
@@ -3,29 +3,21 @@ package io.github.matsimmelbourne.baseline.run;
 import ch.sbb.matsim.mobsim.qsim.SBBTransitModule;
 import ch.sbb.matsim.routing.pt.raptor.SwissRailRaptorModule;
 import org.apache.log4j.Logger;
-import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.network.NetworkWriter;
 import org.matsim.contrib.bicycle.BicycleConfigGroup;
 import org.matsim.contrib.bicycle.Bicycles;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
-import org.matsim.core.config.groups.QSimConfigGroup;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
 import org.matsim.core.network.algorithms.MultimodalNetworkCleaner;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.vehicles.VehicleType;
-import org.matsim.vehicles.VehicleUtils;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class RunBaselineScenario {
@@ -52,7 +44,6 @@ public class RunBaselineScenario {
             this.cleanNetwork=cleanNetwork; //true or false
             this.config = ConfigUtils.loadConfig(configFile, new BicycleConfigGroup());
             config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
-            fillConfigWithBicycleStandardValues(config, false);
 
             this.scenario = ScenarioUtils.loadScenario(config);
 
@@ -73,24 +64,6 @@ public class RunBaselineScenario {
         mode_Set2.add("bicycle");
         new MultimodalNetworkCleaner(network).run(mode_Set2);
         new NetworkWriter(network).write("./networkCleaned.xml.gz");
-    }
-
-    private static void fillConfigWithBicycleStandardValues(Config config, boolean considerMotorizedInteraction){
-
-        BicycleConfigGroup bicycleConfigGroup = (BicycleConfigGroup) config.getModules().get(BicycleConfigGroup.GROUP_NAME);
-        bicycleConfigGroup.setMarginalUtilityOfInfrastructure_m(-0.04);
-        bicycleConfigGroup.setMarginalUtilityOfComfort_m(-0.0002);
-        bicycleConfigGroup.setMarginalUtilityOfGradient_m_100m(-0.02);
-        List<String> mainModeList = new ArrayList<>();
-        mainModeList.add("bicycle");
-        mainModeList.add(TransportMode.car);
-        config.qsim().setMainModes(mainModeList);
-        config.plansCalcRoute().setNetworkModes(mainModeList);
-        config.planCalcScore().addModeParams( new PlanCalcScoreConfigGroup.ModeParams("bicycle").setConstant(0. ).setMarginalUtilityOfDistance(-0.0004 ).setMarginalUtilityOfTraveling(-6.0 ).setMonetaryDistanceRate(0. ) );
-
-        if (considerMotorizedInteraction) {
-            bicycleConfigGroup.setMotorizedInteraction(considerMotorizedInteraction);
-        }
     }
 
 
