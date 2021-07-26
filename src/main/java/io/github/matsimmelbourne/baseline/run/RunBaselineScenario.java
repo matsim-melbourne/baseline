98ef77ecd34d67794af2b66bc3d60e544de0e7bf
jafshin <af.jafari.1992@gmail.com>
2021-07-26 13:41:36 +1000

    removing bicycle config from the baseline

--- src/main/java/io/github/matsimmelbourne/baseline/run/RunBaselineScenario.java
+++ src/main/java/io/github/matsimmelbourne/baseline/run/RunBaselineScenario.java
@@ -8,8 +8,6 @@ import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.network.NetworkWriter;
-import org.matsim.contrib.bicycle.BicycleConfigGroup;
-import org.matsim.contrib.bicycle.Bicycles;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
@@ -50,52 +48,28 @@ public class RunBaselineScenario {
             this.configFile = inputDir + "/" + configName; // e.g. "./scenario/v1.1/config.xml"
             this.inputDir = inputDir;
             this.cleanNetwork=cleanNetwork; //true or false
-            this.config = ConfigUtils.loadConfig(configFile, new BicycleConfigGroup());
+            this.config = ConfigUtils.loadConfig(configFile);
             config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
-            fillConfigWithBicycleStandardValues(config, false);
 
             this.scenario = ScenarioUtils.loadScenario(config);
 
             if (cleanNetwork.equals("true")) {
                 System.out.println("About to clean the network");
                 this.network = this.scenario.getNetwork();
-                cleanNetworkForCarBicycle(network);
+                cleanNetworkForCar(network);
             }
             this.controler = new Controler(scenario);
         }
 
-    private static void cleanNetworkForCarBicycle(Network network){
+    private static void cleanNetworkForCar(Network network){
         System.out.println("cleaning the network");
         Set<String> mode_Set = new HashSet<String>();
         mode_Set.add("car");
         new MultimodalNetworkCleaner(network).run(mode_Set);
-        Set<String> mode_Set2 = new HashSet<String>();
-        mode_Set2.add("bicycle");
-        new MultimodalNetworkCleaner(network).run(mode_Set2);
-        new NetworkWriter(network).write("./networkCleaned.xml.gz");
+        new NetworkWriter(network).write("./networkCleaned4Car.xml.gz");
     }
-
-    private static void fillConfigWithBicycleStandardValues(Config config, boolean considerMotorizedInteraction){
-
-        BicycleConfigGroup bicycleConfigGroup = (BicycleConfigGroup) config.getModules().get(BicycleConfigGroup.GROUP_NAME);
-        bicycleConfigGroup.setMarginalUtilityOfInfrastructure_m(-0.04);
-        bicycleConfigGroup.setMarginalUtilityOfComfort_m(-0.0002);
-        bicycleConfigGroup.setMarginalUtilityOfGradient_m_100m(-0.02);
-        List<String> mainModeList = new ArrayList<>();
-        mainModeList.add("bicycle");
-        mainModeList.add(TransportMode.car);
-        config.qsim().setMainModes(mainModeList);
-        config.plansCalcRoute().setNetworkModes(mainModeList);
-        config.planCalcScore().addModeParams( new PlanCalcScoreConfigGroup.ModeParams("bicycle").setConstant(0. ).setMarginalUtilityOfDistance(-0.0004 ).setMarginalUtilityOfTraveling(-6.0 ).setMonetaryDistanceRate(0. ) );
-
-        if (considerMotorizedInteraction) {
-            bicycleConfigGroup.setMotorizedInteraction(considerMotorizedInteraction);
-        }
-    }
-
 
     public void run(){
-        Bicycles.addAsOverridingModule(controler);
         controler.addOverridingModule(new SBBTransitModule());
         controler.addOverridingModule(new SwissRailRaptorModule());
         controler.run();
